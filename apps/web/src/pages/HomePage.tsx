39f7ff468e5cce39521549c698bc0b2342fe77e5
Mungjin01 <beckaljm@naver.com>
2025-05-21 21:03:50 +0900

    style(apps/web): 임시 홈페이지 내용 추가

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
+import styled from '@emotion/styled';
 import { Header } from '@web/components/common/Header';
+import { SessionBoard } from '@web/components/SessionPage/SessionBoard';
 
 export const HomePage = () => {
   const isElectron = typeof window !== 'undefined' && !!window.electronAPI;
@@ -21,6 +23,19 @@ export const HomePage = () => {
           </button>
         </div>
       )}
+      <BoardContainer>
+        <SessionBoard />
+      </BoardContainer>
     </div>
   );
 };
+
+const BoardContainer = styled.div`
+  display: flex;
+  padding: 136px 64px 80px 64px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 48px;
+  align-self: stretch;
+  width: 100%;
+`;
